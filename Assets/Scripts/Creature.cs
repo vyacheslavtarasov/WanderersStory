0457b92dea4804ece6370970db5f30ccceb63d8f
Vyacheslav Tarasov <aihal21@yandex.ru>
2024-04-12 00:10:10 +0300

    Add knight asset Animation tuning Split Hero component into Hero and Creature to make enemy from the Creature.

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,329 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Creature : MonoBehaviour
+{
+    [Header("Horizontal Movement Adjustments")]
+    [SerializeField] protected float _runAccelerationMultiplier = 0.1f;
+    [SerializeField] protected float _maximumHorizontalVelocity = 4.5f;
+    [SerializeField] protected float _runBrakingMultiplier = 0.8f;
+
+    [SerializeField] protected float _runAccelerationMultiplierSlope = 0.5f;
+    [SerializeField] protected float _maximumHorizontalVelocitySlopeAscend = 4.5f;
+    [SerializeField] protected float _maximumHorizontalVelocitySlopeDescend = 4.5f;
+    [SerializeField] protected float _runBrakingMultiplierSlope = 0.1f;
+
+    [Space]
+    [SerializeField] protected float _attackDamage = -1.0f;
+    [SerializeField] protected float _damageForceToInflict = 200.0f;
+
+    // Necessary information
+    protected Vector3 _direction;
+    protected bool _isGrounded = false; // if the hero is on the ground
+    protected bool _priorGrounded = false;
+    protected bool _runParticleAvailable = false;
+    protected bool _isJumping = false; // if the jump pressed
+    protected int _currentJumpsCount = 0;
+    protected bool _jumpAvailable = true; // we need to avoid secondary jumps right after landing when jump button is kept pressed
+    protected float _fallTime = 0.0f;
+    protected float _ascendTime = 0.0f;
+
+    [Space]
+    [Header("Jump Adjustments")]
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _minimalAscendTime = 1.0f;
+    [SerializeField] protected int _jumpsAmount = 2;
+
+    [Space]
+    [Header("Sensors")]
+    [SerializeField] protected Sensor _attackSensor;
+    [SerializeField] protected Sensor GroundChecker;
+
+    protected Animator _animator;
+    protected Rigidbody2D _rigidbody;
+    protected Health _healthComponent;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _healthComponent = GetComponent<Health>();
+    }
+    public void SetDirection(Vector3 newDirection)
+    {
+        _direction = newDirection;
+    }
+
+    public void SetJumping(bool isJumping)
+    {
+        _isJumping = isJumping;
+    }
+
+    public void Attack()
+    {
+        if (_attackSensor != null)
+        {
+            _animator.SetTrigger("attack");
+        }
+    }
+
+    public void DoAttack()
+    {
+        foreach (GameObject obj in _attackSensor.GetIntersectedObjects().ToArray())
+        {
+            Health healthComponennt = obj.GetComponent<Health>();
+            healthComponennt.ChangeHealth(_attackDamage);
+        }
+    }
+    protected void CheckIsGrounded()
+    {
+        _isGrounded = GroundChecker.GetCollisionStatus();
+    }
+
+    protected virtual void OnChangeHealth(float wasHealth, float currentHealth, float overallHealth)
+    {
+        if (wasHealth > currentHealth)
+        {
+            _animator.SetTrigger("hit");
+            this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * _damageForceToInflict, ForceMode2D.Force);
+        }
+    }
+
+    protected void FlipSprite()
+    {
+        if (_direction.x < 0)
+        {
+            // _spriteRenderer.flipX = true;
+            // doing it via transform for dust paritcles position modification
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (_direction.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            // _spriteRenderer.flipX = false;
+        }
+    }
+
+    protected void SetAnimationParameters()
+    {
+
+        if (_direction.x != 0.0f) // button is pressed
+        {
+            _animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            _animator.SetBool("isRunning", false);
+        }
+
+        _animator.SetFloat("verticalVelocity", _rigidbody.velocity.y);
+
+        if (_isGrounded) // button is pressed
+        {
+            _animator.SetBool("isGrounded", true);
+        }
+        else
+        {
+            _animator.SetBool("isGrounded", false);
+        }
+        if (_isJumping) // button is pressed
+        {
+            _animator.SetBool("isJumping", true);
+        }
+        else
+        {
+            _animator.SetBool("isJumping", false);
+        }
+
+        _animator.SetFloat("fallTime", _fallTime);
+        _animator.SetFloat("ascendTime", _ascendTime);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        CheckIsGrounded(); // Now _airborne variable always show if the carrier is grounded.
+
+        if (_rigidbody.velocity.y < 0.1f && !_isGrounded)
+        {
+            _rigidbody.gravityScale = 3.5f;
+        }
+        else
+        {
+            _rigidbody.gravityScale = 2.0f;
+        }
+
+        if (_rigidbody.velocity.y < -0.01f)
+        {
+            _fallTime += 0.02f;
+        }
+
+        if (_rigidbody.velocity.y > 0.01f)
+        {
+            _ascendTime += 0.02f;
+        }
+
+        // Became airborne event
+        // It is here this way because catching it in different compoent
+        // leads to uneven results. (fixed update for different objects can be called in different order)
+        if (_isGrounded == false && _priorGrounded == true)
+        {
+            _ascendTime = 0.0f;
+            _fallTime = 0.0f;
+
+            // became airborne without jumping is basically fall
+            if (!_isJumping)
+            {
+                // we need to do it to avoid inertia at the end of the slope.
+                if (_rigidbody.velocity.y > 0)
+                {
+                    _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0.0f);
+                }
+                else
+                {
+                    _rigidbody.velocity = new Vector2(0.0f, _rigidbody.velocity.y);
+                }
+            }
+        }
+
+        // Horizontal movement.
+        // Parameters depends on whether Hero is on slope or plane
+        // _rigidbody.velocity = new Vector2(_direction.x * 3.0f, _rigidbody.velocity.y );
+
+        float accelerating = _runAccelerationMultiplier;
+        float breaking = _runBrakingMultiplier;
+        float velocityLimit = _maximumHorizontalVelocity;
+
+        bool onPlane;
+        bool ascendSlope = false;
+
+        float xVelocityAbs = Mathf.Abs(_rigidbody.velocity.x);
+        float yVelocityAbs = Mathf.Abs(_rigidbody.velocity.y);
+
+        if (xVelocityAbs > 0.02f && yVelocityAbs > 0.02f && _isGrounded)
+        {
+            onPlane = false;
+            ascendSlope = _rigidbody.velocity.y > 0;
+        }
+        else
+        {
+            onPlane = true;
+        }
+
+        if (onPlane)
+        {
+            accelerating = _runAccelerationMultiplier;
+            breaking = _runBrakingMultiplier;
+            velocityLimit = _maximumHorizontalVelocity;
+        }
+        else
+        {
+            accelerating = _runAccelerationMultiplierSlope;
+            breaking = _runBrakingMultiplierSlope;
+            velocityLimit = ascendSlope ? _maximumHorizontalVelocitySlopeAscend : _maximumHorizontalVelocitySlopeDescend;
+        }
+
+        Vector2 newVelocity = Vector2.zero;
+
+        if (_direction.x >= 0.01f)
+        {
+            newVelocity = new Vector2(_direction.x * (velocityLimit - _rigidbody.velocity.x) * accelerating + _rigidbody.velocity.x, _rigidbody.velocity.y);
+
+            if (Mathf.Abs(velocityLimit - _rigidbody.velocity.x) < 0.1f)
+            {
+                newVelocity = new Vector2(velocityLimit, _rigidbody.velocity.y);
+            }
+        }
+
+        if (_direction.x <= -0.01f)
+        {
+            newVelocity = new Vector2(-_direction.x * (-velocityLimit - _rigidbody.velocity.x) * accelerating + _rigidbody.velocity.x, _rigidbody.velocity.y);
+
+            if (Mathf.Abs(velocityLimit + _rigidbody.velocity.x) < 0.1f)
+            {
+                newVelocity = new Vector2(-velocityLimit, _rigidbody.velocity.y);
+            }
+        }
+
+
+        if (_direction.x < 0.01f && _direction.x > -0.01f && _isGrounded)
+        {
+            if (xVelocityAbs > 0.1f)
+            {
+                newVelocity = new Vector2(_rigidbody.velocity.x, 0) * breaking;
+            }
+            else
+            {
+                newVelocity = Vector2.zero;
+            }
+        }
+        // Avoiding slipping from slipper slopes
+        if (newVelocity.magnitude < 0.1f && _isGrounded && !_isJumping)
+        {
+            _rigidbody.gravityScale = 0;
+            _rigidbody.velocity = Vector2.zero;
+        }
+        else
+        {
+            _rigidbody.velocity = new Vector2(newVelocity.x, _rigidbody.velocity.y);
+        }
+
+        // Jumping
+        // The firt jump must alway be from the ground.
+        if (_isJumping && _jumpAvailable && _isGrounded)
+        {
+            _rigidbody.velocity = Vector2.zero;
+            _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Force);
+            _currentJumpsCount = _jumpsAmount - 1;
+            _jumpAvailable = false;
+            OnJump();
+        }
+
+        if (_isJumping && _currentJumpsCount > 0 && _jumpAvailable && !_isGrounded)
+        {
+            _rigidbody.velocity = new Vector2(this._rigidbody.velocity.x, 0);
+            _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Force);
+            _currentJumpsCount -= 1;
+            _jumpAvailable = false;
+
+            _ascendTime = 0.0f;
+            _fallTime = 0.0f;
+            OnJump();
+        }
+
+        // Avoid unnecessary jumps on ground if you keep button pressed
+        if (!_isJumping && _currentJumpsCount > 0)
+        {
+            _jumpAvailable = true; // if the button is never released
+        }
+
+        if (_isGrounded && !_isJumping)
+        {
+            _jumpAvailable = true;
+            _currentJumpsCount = 0;
+        }
+
+        // Plunge after jump button releasing till zero y velocity
+        if (!_isJumping && _rigidbody.velocity.y > 0 && !_isGrounded && _ascendTime > _minimalAscendTime)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y / 2);
+        }
+
+        if (_rigidbody.velocity.y < -12.0f)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, -12.0f);
+        }
+
+        SetAnimationParameters();
+        FlipSprite();
+
+        _priorGrounded = _isGrounded;
+    }
+
+    protected virtual void OnJump()
+    {
+
+    }
+}
--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -2,91 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hero : MonoBehaviour
+public class Hero : Creature
 {
-    [Header("Movement adjustments")]
-    [SerializeField] private float _runAccelerationMultiplier = 0.1f;
-    [SerializeField] private float _maximumHorizontalVelocity = 4.5f;
-    [SerializeField] private float _runBrakingMultiplier = 0.8f;
-    
-    [SerializeField] private float _runAccelerationMultiplierSlope = 0.5f;
-    [SerializeField] private float _maximumHorizontalVelocitySlopeAscend = 4.5f;
-    [SerializeField] private float _maximumHorizontalVelocitySlopeDescend = 4.5f;
-    [SerializeField] private float _runBrakingMultiplierSlope = 0.1f;
 
-    [Header("Jump adjustments")]
-    [SerializeField] private float _jumpForce;
-    [SerializeField] private float _minimalAscendTime = 1.0f;
-
-
-    private int _currentJumpsCount;
     [SerializeField] private Sensor _interactSensor;
-    [SerializeField] private float _damageForceToInflict = 200.0f;
-    [SerializeField] private Sensor _attackSensor;
-
-
-    private Vector3 _direction;
-
-    private bool _fallDetected = false;
-
-    private bool _isJumping = false; // if the jump pressed
-    private bool _isGrounded = false; // if the hero is on the ground
-    private bool _isGroundForward = false;
-    private bool _jumpAvailable = true; // we need to avoid secondary jumps right after landing when jump button is kept pressed
-    public float _fallTime = 0.0f;
-    public float _ascendTime = 0.0f;
-    private bool _priorGrounded = false;
 
     [Space][Header("Particles")]
     [SerializeField] private SpawnPrefab RunDustParticleSpawner;
     [SerializeField] private SpawnPrefab JumpDustParticleSpawner;
     [SerializeField] private SpawnPrefab SlapTheGroundParticleSpawner;
-    private bool _runParticleAvailable = false;
+    [SerializeField] private ParticleSystem _particleSystem;
 
     private GameSession _session;
+
     
-
-    // [SerializeField] private LayerCollideCheck GroundChecker;
-    [SerializeField] private LayerCollideCheck ForwardGroundChecker;
-
-    [SerializeField] private Sensor GroundChecker;
-
-
-    private Rigidbody2D _rigidbody;
-    private Animator _animator;
-    [SerializeField]  private ParticleSystem _particleSystem;
-    private SpriteRenderer _spriteRenderer;
-    private Health _healthComponent;
-
-    private void Awake()
-    {
-        _animator = GetComponent<Animator>();
-        _rigidbody = GetComponent<Rigidbody2D>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _healthComponent = GetComponent<Health>();
-    }
-
     private void Start()
     {
         QualitySettings.vSyncCount = 1;
         Application.targetFrameRate = 60;
 
-        _currentJumpsCount = 0;
-
         _session = FindObjectOfType<GameSession>();
         _healthComponent.overallHealth = _session.Data.Health;
-        
-
-    }
-
-    public void SetDirection(Vector3 newDirection)
-    {
-        _direction = newDirection;
-    }
-
-    public void SetJumping(bool isJumping)
-    {
-        _isJumping = isJumping;
+        _jumpsAmount = _session.Data.JumpsAmount;
     }
 
     public void Interact()
@@ -101,297 +38,52 @@ public class Hero : MonoBehaviour
         }
     }
 
-    public void Attack()
-    {
-        
-        if (_attackSensor != null)
-        {
-            _animator.SetTrigger("attack");
-            
-        }
-    }
 
-    public void DoAttack()
+    protected override void OnChangeHealth(float wasHealth, float currentHealth, float overallHealth) 
     {
-        Debug.Log(_attackSensor.GetIntersectedObjects());
-        foreach (GameObject obj in _attackSensor.GetIntersectedObjects().ToArray())
-        {
-            Health healthComponennt = obj.GetComponent<Health>();
-            healthComponennt.ChangeHealth(-1.0f);
-        }
-    }
-
-    public void OnChangeHealth(float wasHealth, float currentHealth, float overallHealth) 
-    {
+        base.OnChangeHealth(wasHealth, currentHealth, overallHealth);
         if (wasHealth > currentHealth)
         {
-            _animator.SetTrigger("hit");
             _particleSystem.Play();
-            this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * _damageForceToInflict, ForceMode2D.Force);
         }
 
         _session.Data.Health = currentHealth;
     }
 
-    private void CheckIsGrounded()
+    protected override void OnJump()
     {
-        _isGrounded = GroundChecker.GetCollisionStatus();
-    }
-
-    private void CheckGroundForward() 
-    {
-        _isGroundForward = ForwardGroundChecker.GetCollisionStatus();
-    }
-
-    private void SetAnimationParameters()
-    {
-            
-        if (_direction.x != 0.0f) // button is pressed
-        {
-            _animator.SetBool("isRunning", true);
-        }
-        else
-        {
-            _animator.SetBool("isRunning", false);
-        }
-
-        _animator.SetFloat("verticalVelocity", _rigidbody.velocity.y);
-
-        if (_isJumping) // button is pressed
-        {
-            _animator.SetBool("isJumping", true);
-        }
-        else
-        {
-            _animator.SetBool("isJumping", false);
-        }
-
-        if (_isGrounded) // button is pressed
-        {
-            _animator.SetBool("isGrounded", true);
-        }
-        else
-        {
-            _animator.SetBool("isGrounded", false);
-        }
-
-        _animator.SetFloat("fallTime", _fallTime);
-        _animator.SetFloat("ascendTime", _ascendTime);
+        base.OnJump();
+        JumpDustParticleSpawner.Spawn();
     }
 
     private void LaunchParticles()
     {
-        if (_runParticleAvailable && Mathf.Abs(_rigidbody.velocity.x) > 0.01f && _rigidbody.velocity.y == 0 && _direction.x != 0 && _isGrounded)
+        if (_runParticleAvailable && Mathf.Abs(_rigidbody.velocity.x) > 0.8f && Mathf.Abs(_rigidbody.velocity.y) < 0.01f && _direction.x != 0 && _isGrounded)
         {
             RunDustParticleSpawner.Spawn();
             _runParticleAvailable = false;
         }
 
-        if (_rigidbody.velocity.x == 0 && _direction.x == 0 && _isGrounded)
+        if (Mathf.Abs(_rigidbody.velocity.x) < 0.5f && _isGrounded)
         {
             _runParticleAvailable = true;
         }
-        
     }
 
-    private void FlipSprite()
-    {
-        if (_direction.x < 0)
-        {
-            // _spriteRenderer.flipX = true;
-            // doing it via transform for dust paritcles position modification
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-        else if (_direction.x > 0)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-            // _spriteRenderer.flipX = false;
-        }
-    }
-
-
+ 
     public void OnGroundTouch(GameObject myGameObject) {
         if (_fallTime > 0.4f)
         {
             SlapTheGroundParticleSpawner.Spawn();
         }
     }
-    private void FixedUpdate()
+
+    
+    protected override void FixedUpdate()
     {
-        CheckIsGrounded(); // Now _airborne variable always show if the carrier is grounded.
-        // CheckGroundForward();
+        base.FixedUpdate();
 
-        if (_rigidbody.velocity.y < 0.1f && !_isGrounded)
-        {
-            _rigidbody.gravityScale = 3.5f;
-        }
-        else
-        {
-            _rigidbody.gravityScale = 2.0f;
-        }
-
-        if (_rigidbody.velocity.y < -0.01f)
-        {
-            _fallTime += 0.02f;
-        }
-
-        if (_rigidbody.velocity.y > 0.01f)
-        {
-            _ascendTime += 0.02f;
-        }
-
-        if (_isGrounded == false && _priorGrounded == true)
-        {
-            _ascendTime = 0.0f;
-            _fallTime = 0.0f;
-            if (!_isJumping) {
-                if (_rigidbody.velocity.y > 0)
-                {
-                    _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0.0f);
-                }else
-                {
-                    _rigidbody.velocity = new Vector2(0.0f, _rigidbody.velocity.y);
-                }                
-            }
-        }
-
-        float accelerating = 0.0f;
-        float breaking = 0.0f;
-        float velocityLimit = 0.0f;
-
-        accelerating = _runAccelerationMultiplier;
-        breaking = _runBrakingMultiplier;
-
-        bool onPlane = false;
-        bool ascendSlope = false;
-
-        if (Mathf.Abs(_rigidbody.velocity.x) > 0.02f && Mathf.Abs(_rigidbody.velocity.y) > 0.02f && _isGrounded)
-        {
-            accelerating = _runAccelerationMultiplierSlope;
-            breaking = _runBrakingMultiplierSlope;
-            onPlane = false;
-            if (_rigidbody.velocity.y > 0) {
-                ascendSlope = true;
-                velocityLimit = _maximumHorizontalVelocitySlopeAscend;
-            }
-            else
-            {
-                ascendSlope = false;
-                velocityLimit = _maximumHorizontalVelocitySlopeDescend;
-            }
-        }
-        else
-        {
-            onPlane = true;
-            accelerating = _runAccelerationMultiplier;
-            breaking = _runBrakingMultiplier;
-            velocityLimit = _maximumHorizontalVelocity;
-        }
-
-
-
-        // Horizontal movement.
-        // _rigidbody.velocity = new Vector2(_direction.x * 3.0f, _rigidbody.velocity.y );
-
-        float absoluteXVelocity = Mathf.Abs(_rigidbody.velocity.x);
-        Vector2 newVelocity = Vector2.zero;
-
-        if (_direction.x >= 0.01f)
-        {
-            newVelocity = new Vector2(_direction.x * (velocityLimit - _rigidbody.velocity.x) * accelerating + _rigidbody.velocity.x, _rigidbody.velocity.y);
-
-            if (Mathf.Abs(velocityLimit - _rigidbody.velocity.x) < 0.1f)
-            {
-                newVelocity = new Vector2(velocityLimit, _rigidbody.velocity.y);
-            }
-        }
-        
-        if (_direction.x <= -0.01f)
-        {
-            newVelocity = new Vector2(-_direction.x * (-velocityLimit - _rigidbody.velocity.x) * accelerating + _rigidbody.velocity.x, _rigidbody.velocity.y);
-
-            if (Mathf.Abs(velocityLimit + _rigidbody.velocity.x) < 0.1f)
-            {
-                newVelocity = new Vector2(-velocityLimit, _rigidbody.velocity.y);
-            }
-        }
-        
-
-        if (_direction.x < 0.01f && _direction.x > -0.01f && _isGrounded)
-        {
-            if (absoluteXVelocity > 0.1f)
-            {
-                newVelocity = new Vector2(_rigidbody.velocity.x, 0) * breaking;
-            }
-            else
-            {
-                newVelocity = Vector2.zero;
-
-            }
-
-        }
-        // Avoiding slipping from slipper slopes
-        if (newVelocity.magnitude < 0.1f && _isGrounded && !_isJumping)
-        {
-            _rigidbody.gravityScale = 0;
-            _rigidbody.velocity = Vector2.zero;
-        }
-        else
-        {
-            _rigidbody.velocity = new Vector2(newVelocity.x, _rigidbody.velocity.y);
-        }
-
-
-
-        // Jumping
-        // The firt jump must alway be from the ground.
-        if (_isJumping  && _jumpAvailable && _isGrounded)
-        {
-            _rigidbody.velocity = Vector2.zero;
-            _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Force);
-            _currentJumpsCount = _session.Data.JumpsAmount - 1;
-            _jumpAvailable = false;
-            JumpDustParticleSpawner.Spawn();
-        }
-
-        if (_isJumping && _currentJumpsCount > 0 && _jumpAvailable && !_isGrounded )
-        {
-            _rigidbody.velocity = new Vector2(this._rigidbody.velocity.x, 0);
-            _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Force);
-            _currentJumpsCount -= 1;
-            _jumpAvailable = false;
-            JumpDustParticleSpawner.Spawn();
-            _ascendTime = 0.0f;
-            _fallTime = 0.0f;
-        }
-
-        // Avoid unnecessary jumps on ground if you keep button pressed
-        if (!_isJumping && _currentJumpsCount > 0)
-        {
-            _jumpAvailable = true; // if the button is never released
-        }
-
-        if (_isGrounded && !_isJumping)
-        {
-            _jumpAvailable = true;
-            _currentJumpsCount = 0;
-        }
-
-        // Plunge after jump button releasing till zero y velocity
-        if ( !_isJumping && _rigidbody.velocity.y > 0 && !_isGrounded && _ascendTime > _minimalAscendTime)
-        {
-            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y / 2);
-        }
-
-        if (_rigidbody.velocity.y < -12.0f)
-        {
-            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, -12.0f);
-        }
-
-        SetAnimationParameters();
-        FlipSprite();
         LaunchParticles();
-        _priorGrounded = _isGrounded;
+       
     }
 }
