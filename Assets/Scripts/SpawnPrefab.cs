dae1659352dfc1f6216593838217354ea32e87fb
Vyacheslav Tarasov <aihal21@yandex.ru>
2024-04-18 08:57:15 +0300

    Implement immobile enemy with long and short range attacks.  No AI.  Sensors and Metronomes only) Small folder refactor.

--- Assets/Scripts/SpawnPrefab.cs
+++ Assets/Scripts/SpawnPrefab.cs
@@ -18,4 +18,14 @@ public class SpawnPrefab : MonoBehaviour
         }
         return instance;
     }
+
+    public void SpawnNoReturn()
+    {
+        var instance = Instantiate(Prefab2Spawn, TransformComponent.position, Quaternion.identity);
+        if (inheritScale)
+        {
+            instance.transform.localScale = TransformComponent.lossyScale;
+        }
+        return;
+    }
 }
