a790ef875f65a7dafc3c48139b680706216e303b
vtarasov <vtarasov@nvidia.com>
2024-05-23 16:22:23 +0300

    make teleport smooth

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Teleport : MonoBehaviour
 {
     public Transform place;
+    public float gettingTransparentTime = 1.0f;
+    public float moveTime = 1.0f;
 
     public void SetPlace(Transform p)
     {
@@ -13,6 +15,45 @@ public class Teleport : MonoBehaviour
 
     public void TeleportObject(GameObject obj)
     {
-        obj.transform.position = place.transform.position;
+        StartCoroutine(TeleportMe(obj));
+    }
+
+    
+
+    private IEnumerator MovingCoroutine(GameObject obj)
+    {
+        float currentTime = 0.0f;
+        Vector3 beginCoordinates = obj.transform.position;
+        while (currentTime < moveTime)
+        {
+            Vector3 coordinate = Vector3.Lerp(beginCoordinates, place.position, currentTime / moveTime);
+            obj.transform.position = coordinate;
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        
+    }
+
+    private IEnumerator VanishingCoroutine(GameObject obj, float targetOpacity)
+    {
+        SpriteRenderer spriteRenderer = obj.GetComponent<SpriteRenderer>();
+        float currentTime = 0.0f;
+        float beginningOpacity = spriteRenderer.color.a;
+        while(currentTime < gettingTransparentTime)
+        {
+            float currentOpacity = Mathf.Lerp(beginningOpacity, targetOpacity, currentTime / gettingTransparentTime);
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, currentOpacity);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private IEnumerator TeleportMe(GameObject obj)
+    {
+        yield return StartCoroutine(VanishingCoroutine(obj, 0.0f));
+        obj.SetActive(false);
+        yield return StartCoroutine(MovingCoroutine(obj));
+        obj.SetActive(true);
+        yield return StartCoroutine(VanishingCoroutine(obj, 1.0f)); 
     }
 }
