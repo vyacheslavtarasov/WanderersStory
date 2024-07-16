216af9c7b6ab6199e27c6126005f9655c28bb1a9
Vyacheslav Tarasov <aihal21@yandex.ru>
2024-07-16 21:52:33 +0300

    Camera switchers, fast and ugly.

---
+++ Assets/HorizontalCameraSwitcher.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalCameraSwitcher : MonoBehaviour
+{
+    [SerializeField] private CameraStateController _cameraStateController;
+    [SerializeField] private PolygonCollider2D _collider;
+
+    private void OnValidate()
+    {
+        if (_cameraStateController == null)
+        {
+            _cameraStateController = FindObjectOfType<CameraStateController>();
+        }
+        _collider = GetComponent<PolygonCollider2D>();
+    }
+
+    public void Switch(GameObject gameObject)
+    {
+        _cameraStateController.SwitchToHorizontalCamera(_collider, gameObject);
+        _cameraStateController.CurrentCameraVolume = gameObject;
+    }
+
+    public void SwitchOff(GameObject gameObject)
+    {
+        _cameraStateController.TrySwitchDefault(gameObject);
+    }
+
+
+
+}
--- Assets/Scripts/CameraStateController.cs
+++ Assets/Scripts/CameraStateController.cs
@@ -5,6 +5,11 @@ public class CameraStateController : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     [SerializeField] private CinemachineVirtualCamera _showCamera;
+    [SerializeField] private CinemachineVirtualCamera _verticalCamera;
+    [SerializeField] private CinemachineVirtualCamera _horizontalCamera;
+    [SerializeField] private PolygonCollider2D _defaultCameraVolume;
+
+    public GameObject CurrentCameraVolume = null;
 
     public void SetShowCameraPosition(Vector3 position)
     {
@@ -20,5 +25,29 @@ public class CameraStateController : MonoBehaviour
     public void SetDefaultCamera()
     {
         _animator.SetBool("Show", false);
+    }
+
+    public void SwitchToVerticalCamera(PolygonCollider2D _collider, GameObject _object)
+    {
+        _verticalCamera.GetComponent<CinemachineConfiner2D>().m_BoundingShape2D = _collider;
+        _animator.SetTrigger("Vertical");
+        CurrentCameraVolume = _object;
+
+    }
+
+    public void SwitchToHorizontalCamera(PolygonCollider2D _collider, GameObject _object)
+    {
+        _horizontalCamera.GetComponent<CinemachineConfiner2D>().m_BoundingShape2D = _collider;
+        _animator.SetTrigger("Horizontal");
+        CurrentCameraVolume = _object;
+
+    }
+
+    public void TrySwitchDefault(GameObject _object)
+    {
+        if (CurrentCameraVolume != null && _object == CurrentCameraVolume)
+        {
+            _animator.SetTrigger("Default");
+        }
     }
 }
